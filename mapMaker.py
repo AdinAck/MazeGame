95c2f905a298d4136f19fa0039903b34e5df61c7
Rettdog <30802926+Rettdog@users.noreply.github.com>
2020-10-11 19:33:21 -0700

    fixed mapMaker non-square and wall distance

--- mapMaker.py
+++ mapMaker.py
@@ -20,12 +20,12 @@ class MapMaker:
 
     def makePNG(self,map):
         array = np.zeros((np.size(map,0),np.size(map,1),3))
-        for i in range(np.size(map,0)):
-            for j in range(np.size(map,1)):
-                if map[j,i] == 1:
-                    array[j,i] = [255,255,255]
-                if map[j,i] == 0:
-                    array[j,i] = [0,0,0]
+        for r in range(np.size(map,0)):
+            for c in range(np.size(map,1)):
+                if map[r,c] == 1:
+                    array[r,c] = [255,255,255]
+                if map[r,c] == 0:
+                    array[r,c] = [0,0,0]
         print(array)
         im = Image.fromarray(np.uint8(array))
         im.save("mapTest.png")
@@ -33,40 +33,44 @@ class MapMaker:
     def makeGrid(self,width,height):
         return [[Cell() for i in range(width)] for i in range(height)]
 
-    # def fillCorners(x,y):
-    #     if x%3 == 0 and y%3 == 0:
-    #         return 1
-    #     return 0
-
-    def drawGrid(self, grid):
-        width = 3*len(grid)+1
-        height = 3*len(grid[0])+1
+    #scale is the distance between walls
+    def drawGrid(self, grid, scale):
+        scale+=1
+        width = scale*len(grid)+1
+        height = scale*len(grid[0])+1
         map = np.zeros((width,height))
 
-        # corners = np.vectorize(MapMaker.fillCorners)
-        # corners(map)
+        for r in range(np.size(map,0)):
+            for c in range(np.size(map,1)):
+                if r%scale == 0 and c%scale ==0:
+                    map[r,c] = 1
 
-        for i in range(np.size(map,0)):
-            for j in range(np.size(map,1)):
-                if i%3 == 0 and j%3 ==0:
-                    map[j,i] = 1
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                k = grid[r][c]
+                i = r
+                j = c
+                # if k.northWall:
+                #     map[scale*r,scale*c+1] = 1
+                #     map[scale*r,scale*c+2] = 1
+                # if k.southWall:
+                #     map[scale*r+scale,scale*c+1] = 1
+                #     map[scale*r+scale,scale*c+2] = 1
+                # if k.westWall:
+                #     map[scale*r+1,scale*c] = 1
+                #     map[scale*r+2,scale*c] = 1
+                # if k.eastWall:
+                #     map[scale*r+1,scale*c+scale] = 1
+                #     map[scale*r+2,scale*c+scale] = 1
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                k = grid[j][i]
                 if k.northWall:
-                    map[3*i,3*j+1] = 1
-                    map[3*i,3*j+2] = 1
+                    map[scale*r,scale*c+1:scale*c+scale] = 1
                 if k.southWall:
-                    map[3*i+3,3*j+1] = 1
-                    map[3*i+3,3*j+2] = 1
+                    map[scale*r+scale,scale*c+1:scale*c+scale] = 1
                 if k.westWall:
-                    map[3*j+1,3*i] = 1
-                    map[3*j+2,3*i] = 1
+                    map[scale*r+1:scale*r+scale,scale*c] = 1
                 if k.eastWall:
-                    map[3*j+1,3*i+3] = 1
-                    map[3*j+2,3*i+3] = 1
-
+                    map[scale*r+1:scale*r+scale,scale*c+scale] = 1
 
         print(map)
         return map
@@ -86,4 +90,7 @@ class Cell:
 
 
 mm = MapMaker()
-mm.makePNG(mm.drawGrid(mm.makeGrid(10,10)))
+defaultScale = 2
+defaultWidth = 5
+defaultHeight = 7
+mm.makePNG(mm.drawGrid(mm.makeGrid(defaultWidth,defaultHeight),defaultScale))
